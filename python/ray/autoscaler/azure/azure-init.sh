aca99035f9785b7ee8bcc9fe2acd410fba4c7389
Markus Cozowicz <marcozo@microsoft.com>
2020-03-03 22:17:11 +0000

    fix install script

--- python/ray/autoscaler/azure/azure-init.sh
+++ python/ray/autoscaler/azure/azure-init.sh
@@ -7,14 +7,15 @@ RAY_HEAD_IP=$4
 TYPE=$5
 
 echo "Installing wheel..."
-sudo -u $USERNAME -sh -c 'conda activate $CONDA_ENV ; pip install $WHEEL'
+sudo -u $USERNAME -i /bin/bash -c "conda init /bin/bash"
+sudo -u $USERNAME -i /bin/bash -c "conda activate $CONDA_ENV; pip install $WHEEL"
 
 echo "Setting up service scripts..."
 cat > /home/$USERNAME/ray-head.sh << EOM
 #/bin/sh
 conda activate $CONDA_ENV
 
-NUM_GPUS=`nvidia-smi -L | wc -l`;
+NUM_GPUS=`nvidia-smi -L | wc -l`
 
 ray stop
 ulimit -n 65536 ; ray start --head --redis-port=6379 --object-manager-port=8076 --num-gpus=$NUM_GPUS
@@ -24,7 +25,7 @@ cat > /home/$USERNAME/ray-worker.sh << EOM
 #/bin/sh
 conda activate $CONDA_ENV
 
-NUM_GPUS=`nvidia-smi -L | wc -l`;
+NUM_GPUS=`nvidia-smi -L | wc -l`
 
 ray stop
 ulimit -n 65536 ;  ray start --address=$RAY_HEAD_IP:6379 --object-manager-port=8076 --num-gpus=$NUM_GPUS
@@ -40,11 +41,14 @@ cat > /lib/systemd/system/ray.service << EOM
 [Service]
    Type=simple
    User=$USERNAME
-   ExecStart= /home/$USERNAME/ray-$TYPE.sh
+   ExecStart=/bin/bash /home/$USERNAME/ray-$TYPE.sh
+
+[Install]
+WantedBy=multi-user.target
 EOM
 
 echo "Configure ray to start at boot..."
-update-rc.d ray defaults
+systemctl enable ray
 
 echo "Starting ray..."
 systemctl start ray
