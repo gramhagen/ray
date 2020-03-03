e5d407a03a44263f2cffe553e252503435eacf0a
Markus Cozowicz <marcozo@microsoft.com>
2020-03-03 20:08:45 +0000

    update init script

--- python/ray/autoscaler/azure/azure-init.sh
+++ python/ray/autoscaler/azure/azure-init.sh
@@ -14,17 +14,24 @@ cat > /home/$USERNAME/ray-head.sh << EOM
 #/bin/sh
 conda activate $CONDA_ENV
 
+NUM_GPUS=`nvidia-smi -L | wc -l`;
+
 ray stop
-ulimit -n 65536 ; ray start --head --redis-port=6379 --object-manager-port=8076
+ulimit -n 65536 ; ray start --head --redis-port=6379 --object-manager-port=8076 --num-gpus=$NUM_GPUS
 EOM
 
 cat > /home/$USERNAME/ray-worker.sh << EOM
 #/bin/sh
 conda activate $CONDA_ENV
 
+NUM_GPUS=`nvidia-smi -L | wc -l`;
+
 ray stop
-ulimit -n 65536 ;  ray start --address=$RAY_HEAD_IP:6379 --object-manager-port=8076
+ulimit -n 65536 ;  ray start --address=$RAY_HEAD_IP:6379 --object-manager-port=8076 --num-gpus=$NUM_GPUS
 EOM
+
+chmod +x /home/$USERNAME/ray-head.sh
+chmod +x /home/$USERNAME/ray-worker.sh
 
 cat > /lib/systemd/system/ray.service << EOM
 [Unit]
@@ -39,5 +46,5 @@ EOM
 echo "Configure ray to start at boot..."
 update-rc.d ray defaults
 
-echo "Starting ray"
+echo "Starting ray..."
 systemctl start ray
